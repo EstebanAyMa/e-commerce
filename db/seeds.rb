a9b4464d8665b7ef6a60e57b0778f6045f705af6
esteban <esteban.ayala@lasallistas.org.mx>
2022-05-13 11:53:47 -0500

    Final Update

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,25 +19,25 @@ User.create!(first_name: "Esteban",
 # ----- PRODUCTS -----
 
 # Categories
-categories = %w[cards bath\ bombs soaps wax\ melts]
+categories = %w[bebidas higiene snacks alimentos otros]
 categories.each do |cat_name|
   Category.create!(name: cat_name)
 end
 
-# sub-categories for cards
+# sub-categories for bebidas
 category = Category.find(1)
-sub_categories = %w[birthday valentines wedding mothers\ day fathers\ day]
+sub_categories = %w[coca pepsi fanta squirt delaware]
 sub_categories.each do |sub_cat|
   category.sub_categories.create!(name: sub_cat)
 end
 
-# Add cards
-test_img_path = File.open(File.join(Rails.root, '/app/assets/images/test_card.jpg'))
+# Add first category images
+test_img_path = File.open(File.join(Rails.root, '/app/assets/images/refrescos.jpg'))
 sub_cats = SubCategory.where(category: category)
 sub_cats.each do |sub_cat|
   10.times do |n|
     category.products.create!(sub_category: sub_cat,
-                              name: "#{sub_cat.name.capitalize} Card #{n + 1}",
+                              name: "#{sub_cat.name.capitalize} #{n + 1}",
                               description: Faker::Lorem.sentence(rand(20..60)),
                               price: rand(1..3.5).round(2),
                               quantity: rand(5..20),
@@ -45,18 +45,75 @@ sub_cats.each do |sub_cat|
   end
 end
 
-# Sub-categories for bath bombs
+# Sub-categories for higiene
 category = Category.find(2)
-sub_categories = %w[fruity floral sweet]
+sub_categories = %w[desodorante gel shampoo]
 sub_categories.each { |sub_cat| category.sub_categories.create!(name: sub_cat) }
 
-# Add bath bombs
-test_img_path = File.open(File.join(Rails.root, '/app/assets/images/test_bath_bomb.jpeg'))
+# Add second category images
+test_img_path = File.open(File.join(Rails.root, '/app/assets/images/higienePersonal.png'))
 sub_cats = SubCategory.where(category: category)
 sub_cats.each do |sub_cat|
   10.times do |n|
     category.products.create! sub_category: sub_cat,
-                              name: "#{sub_cat.name.capitalize} Bath Bomb #{n + 1}",
+                              name: "#{sub_cat.name.capitalize} #{n + 1}",
+                              description: Faker::Lorem.sentence(rand(20..60)),
+                              price: rand(1..3.5).round(2),
+                              quantity: rand(5..20),
+                              primary_img: test_img_path
+  end
+end
+
+# Sub-categories for snacks
+category = Category.find(3)
+sub_categories = %w[galletas papas dulces]
+sub_categories.each { |sub_cat| category.sub_categories.create!(name: sub_cat) }
+
+# Add third category images
+test_img_path = File.open(File.join(Rails.root, '/app/assets/images/snacks.jpg'))
+sub_cats = SubCategory.where(category: category)
+sub_cats.each do |sub_cat|
+  10.times do |n|
+    category.products.create! sub_category: sub_cat,
+                              name: "#{sub_cat.name.capitalize} #{n + 1}",
+                              description: Faker::Lorem.sentence(rand(20..60)),
+                              price: rand(1..3.5).round(2),
+                              quantity: rand(5..20),
+                              primary_img: test_img_path
+  end
+end
+
+# Sub-categories for alimentos
+category = Category.find(4)
+sub_categories = %w[carne pollo pescado]
+sub_categories.each { |sub_cat| category.sub_categories.create!(name: sub_cat) }
+
+# Add fourth category images
+test_img_path = File.open(File.join(Rails.root, '/app/assets/images/alimentos.jpeg'))
+sub_cats = SubCategory.where(category: category)
+sub_cats.each do |sub_cat|
+  10.times do |n|
+    category.products.create! sub_category: sub_cat,
+                              name: "#{sub_cat.name.capitalize} #{n + 1}",
+                              description: Faker::Lorem.sentence(rand(20..60)),
+                              price: rand(1..3.5).round(2),
+                              quantity: rand(5..20),
+                              primary_img: test_img_path
+  end
+end
+
+# Sub-categories for otros
+category = Category.find(5)
+sub_categories = %w[juguetes bicicletas pescado]
+sub_categories.each { |sub_cat| category.sub_categories.create!(name: sub_cat) }
+
+# Add third category images
+test_img_path = File.open(File.join(Rails.root, '/app/assets/images/otros.jpg'))
+sub_cats = SubCategory.where(category: category)
+sub_cats.each do |sub_cat|
+  10.times do |n|
+    category.products.create! sub_category: sub_cat,
+                              name: "#{sub_cat.name.capitalize} #{n + 1}",
                               description: Faker::Lorem.sentence(rand(20..60)),
                               price: rand(1..3.5).round(2),
                               quantity: rand(5..20),
